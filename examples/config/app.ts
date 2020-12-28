25460107d39d0165c0ff2954b7652368c3c4b3bc
Young <634046906@qq.com>
2020-12-28 17:55:51 +0800

    feat:axios新增静态create方法

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -16,6 +16,24 @@ axios.defaults.headers.common['test2'] = 123
 //   console.log(res.data)
 // })
 
+// axios({
+//   transformRequest: [(function (data) {
+//     return qs.stringify(data)
+//   }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
+//   transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function (data) {
+//     if (typeof data === 'object') {
+//       data.b = 2
+//     }
+//     return data
+//   }],
+//   url: '/config/post',
+//   method: 'post',
+//   data: {
+//     a: 1
+//   }
+// }).then((res) => {
+//   console.log(res.data)
+// })
 axios({
   transformRequest: [(function (data) {
     return qs.stringify(data)
--- src/axios.ts
+++ src/axios.ts
@@ -1,15 +1,19 @@
-import { AxiosInstance, AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosStatic } from './types'
 import Axios from './core/Axios'
 import { extend } from './helpers/utils'
 import defaultsConfig from './defaults'
+import mergeConfig from './core/mergeConfig'
 /**
  * 创建一个混合类型，让axios实例即可以axios(config)这样子调用,也可以axios.request() qxios.get()这样子调用
  */
-function createInstance(config: AxiosRequestConfig): AxiosInstance {
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config)
   const instance = Axios.prototype.request.bind(context)
   extend(instance, context)
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 const axios = createInstance(defaultsConfig)
+axios.create = function create(config: AxiosRequestConfig) {
+  return createInstance(mergeConfig(defaultsConfig, config))
+}
 export default axios
